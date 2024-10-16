0e3ee620fec49d5ba61a87e8e70497c180d8629e
Eric Galamback <119694594+EricGalamback3Cloud@users.noreply.github.com>
2024-10-15 21:52:44 -0500

    Update main.tf

--- scenarios/aca-internal/terraform/main.tf
+++ scenarios/aca-internal/terraform/main.tf
@@ -37,95 +37,95 @@ module "spoke" {
   tags                                  = var.tags
 }
 
-#module "supportingServices" {
-#  source                              = "./modules/03-supporting-services"
- # workloadName                        = var.workloadName
-#  environment                         = var.environment
-#  location                            = var.location
-#  spokeResourceGroupName              = module.spoke.spokeResourceGroupName
-#  aRecords                            = var.aRecords
-#  hubResourceGroupName                = module.hub.hubResourceGroupName
-#  hubVnetId                           = module.hub.hubVnetId
-#  spokeVnetId                         = module.spoke.spokeVNetId
-#  spokePrivateEndpointSubnetId        = module.spoke.spokePrivateEndpointsSubnetId
-#  containerRegistryPullRoleAssignment = var.containerRegistryPullRoleAssignment
-#  keyVaultPullRoleAssignment          = var.keyVaultPullRoleAssignment
- # clientIP                            = var.clientIP
- # logAnalyticsWorkspaceId             = module.spoke.logAnalyticsWorkspaceId
- # supportingResourceGroupName         = var.supportingResourceGroupName
+module "supportingServices" {
+  source                              = "./modules/03-supporting-services"
+  workloadName                        = var.workloadName
+  environment                         = var.environment
+  location                            = var.location
+  spokeResourceGroupName              = module.spoke.spokeResourceGroupName
+  aRecords                            = var.aRecords
+  hubResourceGroupName                = module.hub.hubResourceGroupName
+  hubVnetId                           = module.hub.hubVnetId
+  spokeVnetId                         = module.spoke.spokeVNetId
+  spokePrivateEndpointSubnetId        = module.spoke.spokePrivateEndpointsSubnetId
+  containerRegistryPullRoleAssignment = var.containerRegistryPullRoleAssignment
+  keyVaultPullRoleAssignment          = var.keyVaultPullRoleAssignment
+  clientIP                            = var.clientIP
+  logAnalyticsWorkspaceId             = module.spoke.logAnalyticsWorkspaceId
+  supportingResourceGroupName         = var.supportingResourceGroupName
 
- # vnetLinks = [
- #   {
-  #    "name"                = module.spoke.spokeVNetName
-  #    "vnetId"              = module.spoke.spokeVNetId
-  #    "resourceGroupName"   = module.spoke.spokeResourceGroupName
-  #    "registrationEnabled" = false
-  #  },
-  #  {
-   #   "name"                = module.hub.hubVnetName
-   #   "vnetId"              = module.hub.hubVnetId
-   #   "resourceGroupName"   = module.hub.hubResourceGroupName
-   #   "registrationEnabled" = false
-  #}]
- # tags = var.tags
-#}
+  vnetLinks = [
+    {
+      "name"                = module.spoke.spokeVNetName
+      "vnetId"              = module.spoke.spokeVNetId
+      "resourceGroupName"   = module.spoke.spokeResourceGroupName
+      "registrationEnabled" = false
+    },
+    {
+      "name"                = module.hub.hubVnetName
+      "vnetId"              = module.hub.hubVnetId
+      "resourceGroupName"   = module.hub.hubResourceGroupName
+      "registrationEnabled" = false
+  }]
+  tags = var.tags
+}
 
-#module "containerAppsEnvironment" {
-#  source                  = "./modules/04-container-apps-environment"
-#  workloadName            = var.workloadName
-#  environment             = var.environment
-#  location                = var.location
-#  spokeResourceGroupName  = module.spoke.spokeResourceGroupName
-#  hubResourceGroupName    = module.hub.hubResourceGroupName
-#  appInsightsName         = var.appInsightsName
-#  hubVnetId               = module.hub.hubVnetId
-#  spokeVnetId             = module.spoke.spokeVNetId
-#  spokeInfraSubnetId      = module.spoke.spokeInfraSubnetId
-#  logAnalyticsWorkspaceId = module.spoke.logAnalyticsWorkspaceId
-#  workloadProfiles        = var.workloadProfiles
-#  tags                    = var.tags
+module "containerAppsEnvironment" {
+  source                  = "./modules/04-container-apps-environment"
+  workloadName            = var.workloadName
+  environment             = var.environment
+  location                = var.location
+  spokeResourceGroupName  = module.spoke.spokeResourceGroupName
+  hubResourceGroupName    = module.hub.hubResourceGroupName
+  appInsightsName         = var.appInsightsName
+  hubVnetId               = module.hub.hubVnetId
+  spokeVnetId             = module.spoke.spokeVNetId
+  spokeInfraSubnetId      = module.spoke.spokeInfraSubnetId
+  logAnalyticsWorkspaceId = module.spoke.logAnalyticsWorkspaceId
+  workloadProfiles        = var.workloadProfiles
+  tags                    = var.tags
 
-#  vnetLinks = [
-#    {
-#      name                = module.spoke.spokeVNetName
-#      vnetId              = module.spoke.spokeVNetId
- #     resourceGroupName   = module.spoke.spokeResourceGroupName
-#      registrationEnabled = false
- #   },
- #   {
- #     name                = module.hub.hubVnetName
- #     vnetId              = module.hub.hubVnetId
- #     resourceGroupName   = module.hub.hubResourceGroupName
- #     registrationEnabled = false
- # }]
-#}
+  vnetLinks = [
+    {
+      name                = module.spoke.spokeVNetName
+      vnetId              = module.spoke.spokeVNetId
+      resourceGroupName   = module.spoke.spokeResourceGroupName
+      registrationEnabled = false
+    },
+    {
+      name                = module.hub.hubVnetName
+      vnetId              = module.hub.hubVnetId
+      resourceGroupName   = module.hub.hubResourceGroupName
+      registrationEnabled = false
+  }]
+}
 
-#module "helloWorldApp" {
-#  source                                  = "./modules/05-hello-world-sample-app"
- # deployApp                               = var.deployHelloWorldSample
- # resourceGroupName                       = module.spoke.spokeResourceGroupName
- # helloWorldContainerAppName              = var.helloWorldContainerAppName
- # containerAppsEnvironmentId              = module.containerAppsEnvironment.containerAppsEnvironmentId
- # containerRegistryUserAssignedIdentityId = module.supportingServices.containerRegistryUserAssignedIdentityId
-#  workloadProfileName                     = var.workloadProfiles != [] ? var.workloadProfiles.0.name : "Consumption"
-#  tags                                    = var.tags
-#}
+module "helloWorldApp" {
+  source                                  = "./modules/05-hello-world-sample-app"
+  deployApp                               = var.deployHelloWorldSample
+  resourceGroupName                       = module.spoke.spokeResourceGroupName
+  helloWorldContainerAppName              = var.helloWorldContainerAppName
+  containerAppsEnvironmentId              = module.containerAppsEnvironment.containerAppsEnvironmentId
+  containerRegistryUserAssignedIdentityId = module.supportingServices.containerRegistryUserAssignedIdentityId
+  workloadProfileName                     = var.workloadProfiles != [] ? var.workloadProfiles.0.name : "Consumption"
+  tags                                    = var.tags
+}
 
 # If you would like to deploy an Application Gateway and have provided your IP address for KeyVault access, leave this module uncommented
 # If you would like to keep your KeyVault private, comment out this module
-#module "applicationGateway" {
-#  source                          = "./modules/06-application-gateway"
- # workloadName                    = var.workloadName
-#  environment                     = var.environment
-#  location                        = var.location
-#  resourceGroupName               = module.spoke.spokeResourceGroupName
-#  keyVaultName                    = module.supportingServices.keyVaultName
-#  appGatewayCertificateKeyName    = var.appGatewayCertificateKeyName
-#  appGatewayFQDN                  = var.appGatewayFQDN
-#  appGatewayPrimaryBackendEndFQDN = module.helloWorldApp.helloWorldAppFQDN
-#  appGatewaySubnetId              = module.spoke.spokeApplicationGatewaySubnetId
-#  appGatewayLogAnalyticsId        = module.spoke.logAnalyticsWorkspaceId
-#  appGatewayCertificatePath       = var.appGatewayCertificatePath
-#  logAnalyticsWorkspaceId         = module.spoke.logAnalyticsWorkspaceId
-#  tags                            = var.tags
-#}
+module "applicationGateway" {
+  source                          = "./modules/06-application-gateway"
+  workloadName                    = var.workloadName
+  environment                     = var.environment
+  location                        = var.location
+  resourceGroupName               = module.spoke.spokeResourceGroupName
+  keyVaultName                    = module.supportingServices.keyVaultName
+  appGatewayCertificateKeyName    = var.appGatewayCertificateKeyName
+  appGatewayFQDN                  = var.appGatewayFQDN
+  appGatewayPrimaryBackendEndFQDN = module.helloWorldApp.helloWorldAppFQDN
+  appGatewaySubnetId              = module.spoke.spokeApplicationGatewaySubnetId
+  appGatewayLogAnalyticsId        = module.spoke.logAnalyticsWorkspaceId
+  appGatewayCertificatePath       = var.appGatewayCertificatePath
+  logAnalyticsWorkspaceId         = module.spoke.logAnalyticsWorkspaceId
+  tags                            = var.tags
+}
