467c876b0215237f398da5eea4897057d50976a6
Eric Galamback <119694594+EricGalamback3Cloud@users.noreply.github.com>
2024-10-16 17:15:46 -0500

    Fixed outputs

--- scenarios/shared/terraform/modules/service-bus/outputs.tf
+++ scenarios/shared/terraform/modules/service-bus/outputs.tf
@@ -1,14 +1,14 @@
 output "servicebus_namespace_id" {
   description = "The ID of the created Service Bus namespace."
-  value       = azurerm_servicebus_namespace.this.id
+  value       = azurerm_servicebus_namespace.sb.id
 }
 
 output "servicebus_topic_id" {
   description = "The ID of the created Service Bus topic."
-  value       = azurerm_servicebus_topic.this.id
+  value       = azurerm_servicebus_topic.sbTopic.id
 }
 
 output "servicebus_subscription_id" {
-  value = var.create_subscription && length(azurerm_servicebus_subscription.this) > 0 ? azurerm_servicebus_subscription.this[0].id : null
+  value = var.create_subscription && length(azurerm_servicebus_subscription.sbSubscription) > 0 ? azurerm_servicebus_subscription.sbSubscription[0].id : null
   description = "The ID of the Service Bus subscription if it exists."
 }
