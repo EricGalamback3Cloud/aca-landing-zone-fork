26100103fd6595b43ab7e0dbfbec2a0d973d141c
Eric Galamback <119694594+EricGalamback3Cloud@users.noreply.github.com>
2024-10-17 17:20:12 -0500

    added remote state

--- stacks/02-spoke/main.tf
+++ stacks/02-spoke/main.tf
@@ -1,3 +1,16 @@
+data "terraform_remote_state" "hub" {
+  backend = "azurerm"
+#   config = {
+#     storage_account_name = "your-hub-storage-account"
+#     container_name       = "terraform-state"
+#     key                  = "hub/tfstate"
+#   }
+}
+
+output "hub_vnet_id" {
+  value = data.terraform_remote_state.hub.outputs.vnet_id
+}
+
 module "spoke" {
   source                                = "./modules/spoke"
   workloadName                          = var.workloadName
