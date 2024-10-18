2ca7177a72c69402684242c524ae9492f7ce8a7d
Eric Galamback <119694594+EricGalamback3Cloud@users.noreply.github.com>
2024-10-18 17:28:38 -0500

    fixed key

--- terraform/stacks/05-applications-and-gateway/main.tf
+++ terraform/stacks/05-applications-and-gateway/main.tf
@@ -34,7 +34,7 @@ data "terraform_remote_state" "container_app_env" {
   config = {
     storage_account_name = var.state_storage_account_name
     container_name       = var.state_container_name
-    key                  = var.supporting_services_key
+    key                  = var.container_app_env_key
     resource_group_name = var.state_resource_group_name
   }
 }
