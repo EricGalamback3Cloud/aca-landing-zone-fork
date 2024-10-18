78fb9824ddd0d0dc7bcfb50bf55c4087badf67e8
Eric Galamback <119694594+EricGalamback3Cloud@users.noreply.github.com>
2024-10-17 21:18:32 -0500

    fixed more variables

--- stacks/05-applications-and-gateway/variables.tf
+++ stacks/05-applications-and-gateway/variables.tf
@@ -36,15 +36,54 @@ variable "helloWorldContainerAppName" {
 }
 
 # Application Gateway
-  appGatewayCertificateKeyName    = var.appGatewayCertificateKeyName
-  appGatewayFQDN                  = var.appGatewayFQDN
-  appGatewayCertificatePath       = var.appGatewayCertificatePath
+variable "appGatewayCertificateKeyName" {}
+variable "appGatewayFQDN" {}
+variable "appGatewayCertificatePath" {}
 
 # Service Bus
-  namespace_name = var.namespace_name
-  topic_name = var.topic_name
-  location = var.location
-  sku = var.sku
-  subscription_name = var.subscription_name
-  max_delivery_count = var.max_delivery_count
-  lock_duration = var.lock_duration
+variable "namespace_name" {
+  description = "The name of the Service Bus namespace."
+  type        = string
+}
+
+variable "topic_name" {
+  description = "The name of the Service Bus topic."
+  type        = string
+}
+
+variable "sku" {
+  description = "The SKU of the Service Bus namespace (Basic, Standard, or Premium)."
+  type        = string
+  default     = "Standard"
+}
+
+variable "enable_partitioning" {
+  description = "Enable partitioning for the topic."
+  type        = bool
+  default     = false
+}
+
+variable "create_subscription" {
+  description = "Whether to create a subscription for the topic."
+  type        = bool
+  default     = false
+}
+
+variable "subscription_name" {
+  description = "The name of the subscription."
+  type        = string
+  default     = "default-subscription" # This will be ignored if create_subscription is false
+}
+
+variable "max_delivery_count" {
+  description = "The maximum number of delivery attempts before the message is dead-lettered."
+  type        = number
+  default     = 10
+}
+
+variable "lock_duration" {
+  description = "The duration for which the message is locked."
+  type        = string
+  default     = "PT1M"
+}
+
