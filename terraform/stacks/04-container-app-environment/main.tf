bd4b787b1b826ee7c9ef5cce17981c6af0779384
Eric Galamback <119694594+EricGalamback3Cloud@users.noreply.github.com>
2024-10-21 18:07:06 -0500

    removed unused module

--- terraform/stacks/04-container-app-environment/main.tf
+++ terraform/stacks/04-container-app-environment/main.tf
@@ -42,17 +42,15 @@ module "applicationInsights" {
 }
 
 module "serviceBus" {
-  source = "./modules/service-bus"
+  source = "../../shared/modules/service-bus"
   namespace_name = var.namespace_name
   topic_name = var.topic_name
-  resourceGroupName = data.terraform_remote_state.spoke.outputs.spokeResourceGroupName
+  resource_group_name = data.terraform_remote_state.spoke.outputs.spokeResourceGroupName
   location = var.location
   sku = var.sku
   enable_partitioning = true
   create_subscription = true
   subscription_name = var.subscription_name
-  max_delivery_count = var.max_delivery_count
-  lock_duration = var.lock_duration
   tags = var.tags
 }
 
