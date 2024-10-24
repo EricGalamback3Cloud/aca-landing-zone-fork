8737e5dc60c8ecad7f6555febcb38faab0cad269
Eric Galamback <119694594+EricGalamback3Cloud@users.noreply.github.com>
2024-10-23 22:38:37 -0500

    trying to create container apps

--- terraform/stacks/05-applications-and-gateway/main.tf
+++ terraform/stacks/05-applications-and-gateway/main.tf
@@ -50,6 +50,69 @@ module "helloWorldApp" {
   tags                                    = var.tags
 }
 
+resource "azurerm_container_app" "publisher" {
+  name                         = "iot-publisher"
+  resource_group_name          = data.terraform_remote_state.spoke.outputs.spokeResourceGroupName
+  container_app_environment_id = data.terraform_remote_state.container_app_env.outputs.containerAppsEnvironmentId
+  tags                         = var.tags
+  workload_profile_name        = var.workloadProfileName
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [data.terraform_remote_state.supporting_services.outputs.containerRegistryUserAssignedIdentityId]
+  }
+
+  revision_mode = "Single"
+
+  template {
+    container {
+      name   = "publisher"
+      cpu    = "0.5"
+      memory = "1Gi"
+      image  = data.terraform_remote_state.supporting_services.outputs.containerRegistryName + ".azurecr.io/iotpublisher:latest"
+    }
+
+    min_replicas = 1
+    max_replicas = 10
+  }
+
+  dapr {
+    app_id  = "iot-publisher"
+  }
+}
+
+resource "azurerm_container_app" "subscriber" {
+  name                         = "iot-subscriber"
+  resource_group_name          = data.terraform_remote_state.spoke.outputs.spokeResourceGroupName
+  container_app_environment_id = data.terraform_remote_state.container_app_env.outputs.containerAppsEnvironmentId
+  tags                         = var.tags
+  workload_profile_name        = var.workloadProfileName
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [data.terraform_remote_state.supporting_services.outputs.containerRegistryUserAssignedIdentityId]
+  }
+
+  revision_mode = "Single"
+
+  template {
+    container {
+      name   = "subscriber"
+      cpu    = "0.5"
+      memory = "1Gi"
+      image  = data.terraform_remote_state.supporting_services.outputs.containerRegistryName + ".azurecr.io/iotsubscriber:latest"
+    }
+
+    min_replicas = 1
+    max_replicas = 10
+  }
+
+  dapr {
+    app_id  = "iot-subscriber"
+    app_port = 8080
+  }
+}
+
 # If you would like to deploy an Application Gateway and have provided your IP address for KeyVault access, leave this module uncommented
 module "applicationGateway" {
   source                          = "./modules/application-gateway"
